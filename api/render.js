ce8393932caad8dbf6fc1075d390dd64632e4bfa
forgor0x10 <burdigindanil@gmail.com>
2025-08-26 16:38:44 +0500

    feat: complete rewrite

---
+++ api/render.js
@@ -0,0 +1,38 @@
+import { readFile } from "fs/promises";
+
+export default async function handler(req, res) {
+  const { pageName } = req.query;
+
+  let data = await readFile(`templates/${pageName || "400"}.html`, "utf8");
+
+  if (pageName === "index") {
+    const lastfmParams = {
+      method: "user.getrecenttracks",
+      user: "forgorOx1O",
+      api_key: process.env.LASTFM_API_KEY,
+      format: "json",
+      limit: "1",
+    };
+
+    const lastfmQueryParams = new URLSearchParams(lastfmParams).toString();
+
+    try {
+      const response = await fetch(
+        `https://ws.audioscrobbler.com/2.0/?${lastfmQueryParams}`
+      );
+      const json = await response.json();
+
+      const track = json.recenttracks?.track?.[0];
+      const lastfmTrack = track
+        ? `${track.artist["#text"]}<br />${track.name}`.toLowerCase()
+        : "Unknown";
+
+      data = data.replace("{{lastfm-track}}", lastfmTrack);
+    } catch (err) {
+      data = data.replace("{{lastfm-track}}", `error: ${err.message}`);
+    }
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).send(data);
+}
