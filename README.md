8244a79598e9cedc2feffa7326fd08f7257b35e9
forgor0x10 <burdigindanil@gmail.com>
2026-03-15 17:45:00 +0500

    feat: remake styling

--- README.md
+++
@@ -1,5 +0,0 @@
-# website
-
-A little website with absolutely no dependencies
-
-A custom router is implemented in `/api/route.js`
--- api/render.js
+++ api/render.js
@@ -8,43 +8,42 @@ const dirname = path.dirname(filename);
 const pagesDir = path.join(dirname, "../pages");
 const fileEncoding = "utf-8";
 
-async function getLastTrack() {
-  let lastTrack;
+async function getLastTracks() {
+  let lastTracks;
 
   try {
     const responseParams = {
       method: "user.getrecenttracks",
       api_key: process.env.LASTFM_API_KEY,
       format: "json",
-      limit: 1,
+      limit: 5,
       user: "forgorOx1O",
     };
 
     const responseQuery = new URLSearchParams(responseParams).toString();
     const response = await fetch(
-      `http://ws.audioscrobbler.com/2.0/?${responseQuery}`
+      `http://ws.audioscrobbler.com/2.0/?${responseQuery}`,
     );
 
     if (!response.ok) {
-      return lastTrack;
+      return lastTracks;
     }
 
     const json = await response.json();
-    const track = json?.recenttracks?.track?.[0];
+    const tracks = json?.recenttracks?.track;
 
-    const artist = track?.artist?.["#text"];
-    const title = track?.name;
-
-    if (!artist || !title) {
-      return lastTrack;
-    }
-
-    lastTrack = `${artist} - ${title}`;
+    lastTracks = tracks
+      .map((track) => {
+        const artist = track?.artist?.["#text"];
+        const title = track?.name;
+        return `${artist} - ${title}`.toLowerCase();
+      })
+      .join("<br />");
   } catch (err) {
-    lastTrack = "Error! Reloading should help";
+    lastTracks = "Error. Reloading should help";
   }
 
-  return lastTrack;
+  return lastTracks;
 }
 
 export default {
@@ -53,7 +52,7 @@ export default {
 
     try {
       let content = await fs.readFile(filePath, fileEncoding);
-      content = content.replace("{lastTrack}", await getLastTrack());
+      content = content.replaceAll("{lastTracks}", await getLastTracks());
       return new Response(content, {
         status: 200,
         headers: { "Content-Type": "text/html" },
